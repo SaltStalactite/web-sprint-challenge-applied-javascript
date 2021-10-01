9fba05cacdecf118d5a601c3f2a225ba0bf81c89
cc <votaryofturms@icloud.com>
2021-10-01 16:06:03 -0400

    appended topics tabs div to tabs-container div

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,3 +1,6 @@
+import axios from "axios";
+import { response } from "msw";
+
 const Tabs = (topics) => {
   // TASK 3
   // ---------------------
@@ -13,6 +16,55 @@ const Tabs = (topics) => {
   //   <div class="tab">technology</div>
   // </div>
   //
+  const topicsDiv = document.createElement('div');
+  topicsDiv.classList.add('topics');
+  // topics.forEach(i => {
+  //   const tabDiv = document.createElement('div')
+  //   tabDiv.classList.add('tab')
+  //   tabDiv.textContent = [i]
+  //   topicsDiv.appendChild(tabDiv)
+  // })
+  // console.log(topicsDiv)
+  // return topicsDiv
+  topics.forEach(topic => {
+    const tabDiv = document.createElement('div')
+    tabDiv.classList.add('tab')
+    tabDiv.textContent = topic
+    topicsDiv.appendChild(tabDiv)
+  })
+
+  console.log(topicsDiv)
+  return topicsDiv
+
+
+
+
+
+  // const topicTabs = topics.forEach(topic => {
+  //   document.createElement('div').textContent = topic
+  //   return topicTabs
+  // })
+
+  // topics.forEach(topic => {
+  //   const topicItem = document.createElement('div')
+  //   topicItem.textContent = topic
+  //   topicsDiv.appendChild(topicItem)
+  // })
+
+  // axios.get('http://localhost:5000/api/topics')
+  //   .then(res => {
+  //     console.log(res.data)
+  //     for (let i = 0; i < res.data.length; i++) {
+  //       topics[i] = res.data[i]
+  //       return topics
+  //     }
+  //   })
+  //   .catch(err => {
+  //     console.error(err);
+  //   })
+  //   .finally(() => {
+  //     console.log('get resquest')
+  //   })
 }
 
 const tabsAppender = (selector) => {
@@ -23,6 +75,32 @@ const tabsAppender = (selector) => {
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
+
+  axios.get('http://localhost:5000/api/topics')
+    .then(res => {
+      let topics = []
+      // topics.push(res.data.topics)
+      res.data.topics.forEach(topic => {
+        topics.push(topic)
+      })
+      console.log(res.data.topics)
+      console.log(topics)
+      const tabsElement = Tabs(topics)
+      console.log(tabsElement)
+      document.querySelector(selector).appendChild(tabsElement)
+    })
+    .catch(err => {
+      console.error(err)
+    })
+    .finally(() => [
+      console.log('get request')
+    ])
+  const topics = []
+  console.log(topics)
+
+  // const tabsElement = Tabs(topics)
+  // console.log(tabsElement)
+  // document.querySelector(selector).appendChild(tabsElement)
 }
 
 export { Tabs, tabsAppender }
